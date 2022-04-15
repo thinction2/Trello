264a5938cd7cf176ab62ccbe2c78121490252344
unknown <dudwlsehdwk@nate.com>
2022-04-15 16:57:00 +0900

    2022.04.15 setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { darkTheme } from "./theme";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = createRoot(document.getElementById("root") as HTMLElement);
+const queryClient = new QueryClient();
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <HelmetProvider>
+          {/* @ts-ignore */}
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
+        </HelmetProvider>
+      </ThemeProvider>
+    </RecoilRoot>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/styled.d.ts
@@ -0,0 +1,9 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    textColor: string;
+    backgroundColor: string;
+    accentColor: string;
+  }
+}
